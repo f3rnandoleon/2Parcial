4945654d864c3ec74aaf01baf45ca62a21302ce0
fernando leon <ferleonqaq@gmail.com>
2022-10-21 11:54:19 -0400

    tercer componente

---
+++ src/componentes/componente3.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/cpmponent3.css"
+
+function componente3() {
+  return (
+    <div class="action_button">
+      <button class="boton1">
+          Follow
+      </button>
+      <button class="boton2">
+          
+      </button>
+    </div>
+  )
+}
+
+export {componente3}
